5e2859630064943b5633852199f3de0ebcc46cbc
drewgolas <drewgolas@gmail.com>
2020-07-30 10:38:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const Discord = require('discord.io');
+const logger = require('winston');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+const bot = new Discord.Client({
+   token: process.env.BOT_TOKEN,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')\n');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        let args = message.substring(1).split(' ');
+        const cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            case 'quotas':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Here are the quotas:'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
